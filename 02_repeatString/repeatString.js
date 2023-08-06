950218b384374df29248e88341bc17e8baf41aff
a.mihnov <a.mihnov@anlax.org>
2023-08-06 08:06:09 +0300

    second exercise is done wtih solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, item) {
+    if (item < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < item; i++){
+        string += word;
+    }
+    return string;
 };
 
 // Do not edit below this line
