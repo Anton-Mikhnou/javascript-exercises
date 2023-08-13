da0aff43cb27ae3305aa7c954388d379147e224b
a.mihnov <a.mihnov@anlax.org>
2023-08-13 10:22:33 +0300

    Works first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(numberOne, numberTwo) {
+    let sum = 0;
+    for (numberOne; numberOne <= numberTwo; numberOne++){
+    sum += numberOne;
+    }
+    return sum
 };
+sumAll(123, 1);
 
 // Do not edit below this line
 module.exports = sumAll;
