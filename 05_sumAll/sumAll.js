3b5ce749634746876ab5a6b2364c9590f373e2e2
a.mihnov <a.mihnov@anlax.org>
2023-08-15 22:41:15 +0300

    5 exercise is done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,17 +1,22 @@
 const sumAll = function(numberOne, numberTwo) {
     let sum = 0;
-    if (numberOne <= numberTwo){
-        for (numberOne; numberOne <= numberTwo; numberOne++){
-        sum += numberOne;
+    let i = numberOne;
+    let j = numberTwo;
+    if(typeof numberOne == 'string' || typeof numberTwo == 'string' || Array.isArray(numberOne) || Array.isArray(numberTwo) || numberOne < 0 || numberTwo < 0){
+        return "ERROR";
+    }
+    else if (i <= j){
+        for (i;i <= j;i++){
+        sum += i;
         }
-    } else if (numberOne >= numberTwo){
-        for (numberTwo; numberTwo <= numberOne; numberTwo++){
-        sum += numberTwo;
+    } else if (i >= j){
+        for (j; j <= i; j++){
+        sum += j;
         }
     }
     return sum;
 };
-sumAll(123, 1);
+sumAll([90, 1], 10);
 
 // Do not edit below this line
 module.exports = sumAll;
