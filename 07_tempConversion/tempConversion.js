210b8d822dad6dd22f474c783054a213a4ba6c87
a.mihnov <a.mihnov@anlax.org>
2023-08-16 20:34:47 +0300

    7 exercise is done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let value = (fahrenheit - 32) * 5/9;
+  return +value.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let value = (celsius * 9/5) + 32;
+  return +value.toFixed(1);
 };
-
+convertToCelsius(-100);
+convertToFahrenheit(-10);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
