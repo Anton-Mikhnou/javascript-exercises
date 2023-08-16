addc8b82831009fb382a25185d25726687cda178
a.mihnov <a.mihnov@anlax.org>
2023-08-16 23:11:10 +0300

    8 exercise is done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,42 @@
-const add = function() {
-	
+const add = function(first, second) {
+	return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let res = array.reduce(function(sum, elem){
+    return sum + elem;
+  }, 0)
+  return res;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let res = array.reduce(function(sum, elem){
+    return sum * elem;
+  }, 1)
+  return res;
 };
 
-const power = function() {
-	
+const power = function(first, second) {
+	return first ** second;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0){
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
 };
+// add(2,6);
+// subtract(10, 4);
+// sum([1, 3, 5, 7, 9]);
+// multiply([2, 4, 6, 8, 10, 12, 14]);
+// power(4, 3);
+factorial(10);
 
 // Do not edit below this line
 module.exports = {
