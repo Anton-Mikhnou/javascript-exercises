1232651974accba417018fd9a33661343429d8fd
a.mihnov <a.mihnov@anlax.org>
2023-08-17 23:31:47 +0300

    9 exercise is done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let cleanWord = word.toLowerCase().replace(/[^\w]/g, "");
+    if (cleanWord === cleanWord.split("").reverse().join("")){
+        return true;
+    } return false;
 };
+
+palindromes('r3ace3car');
 
 // Do not edit below this line
 module.exports = palindromes;
