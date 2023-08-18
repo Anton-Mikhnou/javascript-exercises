3235bb8ac18bd6677ec3332638fbae10fe4359a8
a.mihnov <a.mihnov@anlax.org>
2023-08-18 22:15:26 +0300

    10 exercise is done with hejp YouTube

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (num ===0) return 0; 
 
+    let a = 1;
+        b = 1;
+
+    for (let i = 3; i <= num; i++){
+        let c =a + b;
+            a = b;
+            b = c;
+    }
+    return b;
 };
+
+fibonacci("8");
 
 // Do not edit below this line
 module.exports = fibonacci;
